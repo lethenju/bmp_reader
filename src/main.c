caf4bfa07436c3742e598b9dfef9d142dab78d48
Julien LE THENO <julien.letheno@gmail.com>
2019-02-18 15:00:28 +0100

    fix structs WIP

--- src/main.c
+++ src/main.c
@@ -7,9 +7,9 @@
 #define MAXBUF 10000000
 
 typedef struct pixel_t {
-    unsigned int r; //RED
-    unsigned int g; //GREEN
-    unsigned int b; //BLUE
+    int r; //RED
+    int g; //GREEN
+    int b; //BLUE
 } pixel;
 
 typedef struct bmp_reader_context_t {
@@ -69,44 +69,40 @@ int main(int argc, char* argv[]) {
     printf("Size DIB header : %lu\n",sizeDIB);
     printf("width : %lu\n",ctx->width);
     printf("height : %lu\n",ctx->height);
-    
-    int screen[ctx->width][ctx->height]; 
+
     ptr = buf+offset;
     float x,y;
-    ctx->image_ptr = (pixel**) malloc(ctx->width*ctx->height*sizeof(pixel));
-
-    for (y= ctx->height-1 ; y>=0; y-=1){
-        for (x=0; x<ctx->width; x+=1){
-            pixel **image = ctx->image_ptr;
-            int num = x*y+x;
-            pixel *p = (pixel*) malloc(sizeof(pixel));
+    int k = 0;
+    ctx->image_ptr = malloc(ctx->height);
+    for(y=0; y < HEIGHT; y++){
+        pixel *line = malloc(ctx->width*sizeof(pixel));
+        pixel *p =  *(ctx->image_ptr)+(int)y;
+        p = line;
+    }
+    printf("lol");
+    for (y= 0 ; y<HEIGHT; y++){
+        pixel *line = *(ctx->image_ptr) + (int)y;
+        for (x=0; x<WIDTH ; x++){
+            pixel *p = malloc(sizeof(pixel));
+            p = line + (int)x;
             p->r = *ptr;
             p->g = *(ptr+1);
             p->b = *(ptr+2);
-            memcpy((void*) (image+num), (void*) p, sizeof(pixel));
-            screen[(int)floor(x*WIDTH/ctx->width)][(int)floor(y*HEIGHT/ctx->height)] = 
-                abs((*ptr+*(ptr+1)+*(ptr+2))/(3*20));
             ptr+=3;
         }
     }
+    k=0;
     for (y= 0 ; y<HEIGHT; y++){
+        pixel **line = ctx->image_ptr + (int)y;
         for (x=0; x<WIDTH; x++){
-            int num = x*y+x;
-            pixel **image = ctx->image_ptr;
-            pixel *p = *(image+num);
+            pixel *p = *line+(int)x;
             int average = abs((p->b+p->g+p->r)/(3*20));
 
             if (average >= 0 && average < 10) {
-                printf("%c",ascii_tran[screen[(int)x][(int)y]]);
+                printf("%c",ascii_tran[average]);
             } else {
                 printf(" ");
             }
-          /*  if (screen[(int)x][(int)y] >= 0 && screen[(int)x][(int)y]<10) {
-                printf("%c",ascii_tran[screen[(int)x][(int)y]]);
-            } else {
-                printf(" ");
-            }
-            */
         }
         printf("\n");
     }
